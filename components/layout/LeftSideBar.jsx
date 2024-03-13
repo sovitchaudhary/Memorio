8bb7a6b18ec64e765b8a6886a27231d074935d23
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-03-14 04:31:49 +0530

    Design of home page layout

---
+++ components/layout/LeftSideBar.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const LeftSideBar = () => {
+  return <div >
+         </div>;
+};
+
+export default LeftSideBar;
---
+++ components/layout/Menu.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Menu
