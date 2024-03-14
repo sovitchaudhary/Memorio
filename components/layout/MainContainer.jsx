cacfa80f62a2c827b28945f44c7bd946991c4376
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-03-14 13:14:46 +0530

    Design Topbar

--- components/layout/MainContainer.jsx
+++ components/layout/MainContainer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Topbar from './Topbar'
 
-const MainContainer = () => {
+const MainContainer = (children) => {
   return (
-    <div>MainContainer</div>
+    <section className='flex flex-col flex-1 max-w-3xl px-4 md:px-10 lg:px-4 xl:px-20'>
+      <Topbar />
+    </section>
   )
 }
 
--- components/layout/Topbar.jsx
+++ components/layout/Topbar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Add, Search } from "@mui/icons-material";
+import { useRouter } from "next/navigation";
+import { SignOutButton, SignedIn, UserButton } from "@clerk/nextjs";
+import { Logout } from "@mui/icons-material";
+import Link from "next/link";
+import Image from "next/image";
+
 
 const Topbar = () => {
-  return (
-    <div>Topbar</div>
-  )
-}
+  const router = useRouter();
+  const [search, setSearch] = useState("");
 
-export default Topbar
+  return (
+    <div className="flex justify-between items-center mt-6">
+      <div className="relative">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search posts, people, ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Search className="search-icon" onClick={() => {}} />
+      </div>
+      <button className="create-post-btn" onClick={() => router.push("/create-post")}>
+        <Add /> <p>Create A Post</p>
+      </button>
+
+      <div className="flex gap-3">
+        <SignedIn>
+          <SignOutButton>
+            <div className="flex cursor-pointer items-center md:hidden">
+              <Logout sx={{ color: "white", fontSize: "32px" }} />
+            </div>
+          </SignOutButton>
+        </SignedIn>
+        <Link href="/">
+        <Image
+          src="/assets/sovit.jpeg"
+          alt="profile photo"
+          width={50}
+          height={50}
+          className="rounded-full md:hidden"
+        />
+      </Link>
+      </div>
+
+    </div>
+  );
+};
+
+export default Topbar;
